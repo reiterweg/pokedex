649bb69248eaf2a5d201d2a3b0316c918d5956ed
Alejandro Diaz Carmona <alejandro.diaz@quienmepresta.com>
2016-09-21 21:25:06 -0500

    Add a controller and implement basic binding.

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+(function() {
+  var app = angular.module('pokedex', []);
+
+  app.controller('PokemonController', function() {
+  	this.pokemon = {
+      id: '001',
+      name: 'Bulbasaur',
+      species: 'Seed Pokémon',
+      type: [ 'Grass' , 'Poison' ],
+      height: "2'4\" (0.71m)",
+      weight: "15 lbs (6.9 kg)",
+      abilities: [ "Overgrow", "Chlorophyll" ]
+  	};
+  });
+})();
