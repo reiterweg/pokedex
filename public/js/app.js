b13d44984db24a8d4b6b35e3cf7a49ce7a73d959
Alejandro Diaz Carmona <alejandro.diaz@quienmepresta.com>
2016-09-23 07:43:38 -0500

    Improve tab functionality.

--- public/js/app.js
+++ public/js/app.js
@@ -22,4 +22,12 @@
       evolution: [ "Bulbasaur", "Ivysaur", "Venusaur" ]
   	};
   });
+
+  app.controller('TabsController', function() {
+    this.tab = 1;
+
+    this.selectTab = function(tab) {
+      this.tab = tab;
+    };
+  });
 })();
