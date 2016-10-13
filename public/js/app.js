9a4b2268956a799e397c14be965847995e9415a1
Alejandro Diaz Carmona <alejandro.diaz@quienmepresta.com>
2016-10-12 23:15:31 -0500

    Create directive for comments with controller.

--- public/js/app.js
+++ public/js/app.js
@@ -32,28 +32,6 @@
     };
   });
 
-  app.controller('CommentsController', function() {
-    this.comments = [];
-    this.comment = {};
-    this.show = false;
-
-    this.toggle = function() {
-      this.show = !this.show;
-    };
-
-    this.anonymousChanged = function() {
-      if (this.comment.anonymous) {
-        this.comment.email = "";
-      }
-    };
-
-    this.addComment = function() {
-      this.comment.date = Date.now();
-      this.comments.push(this.comment);
-      this.comment = {};
-    };
-  });
-
   app.filter('imageify', function() {
     return function(input) {
       return "img/pokemons/" + input.toLowerCase() + ".jpg";
@@ -92,6 +70,35 @@
     return {
       restrict: 'E',
       templateUrl: 'partials/pokemon-evolution.html'
+    };
+  })
+
+  app.directive('pokemonComments', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/pokemon-comments.html',
+      controller: function() {
+        this.comments = [];
+        this.comment = {};
+        this.show = false;
+
+        this.toggle = function() {
+          this.show = !this.show;
+        };
+
+        this.anonymousChanged = function() {
+          if (this.comment.anonymous) {
+            this.comment.email = "";
+          }
+        };
+
+        this.addComment = function() {
+          this.comment.date = Date.now();
+          this.comments.push(this.comment);
+          this.comment = {};
+        };
+      },
+      controllerAs: 'cmtsCtrl'
     };
   })
 
