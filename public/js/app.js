6809273c5fdd59c1cafbd9b854e2139013545a26
Alejandro Diaz Carmona <alejandro.diaz@quienmepresta.com>
2016-10-13 23:21:18 -0500

    Create pokedex view.

--- public/js/app.js
+++ public/js/app.js
@@ -10,9 +10,15 @@
   app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/', {
+        templateUrl: 'views/pokedex.html'
+      })
+      .when('/pokemon/:id', {
         templateUrl: 'views/pokemon.html',
         controller: 'PokemonController',
         controllerAs: 'pkmCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
       })
   }]);
 
