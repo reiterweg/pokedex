3046926a19f958d849c3e1ad7a571be640ea7d16
Alejandro Diaz Carmona <alejandro.diaz@quienmepresta.com>
2016-10-14 00:09:05 -0500

    Add filter to normalize images.

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,10 +1,19 @@
 (function() {
 
   angular.module('pokedex.filters', [])
-    .filter('imageify', function() {
+    .filter('normalize', function() {
       return function(input) {
-        return "img/pokemons/" + input.toLowerCase() + ".jpg";
+        return input
+          .replace('♀', 'f')
+          .replace('♂', 'm')
+          .replace(/\W+/g, '')
+          .toLowerCase();
       };
-    });
+    })
+    .filter('imageify', ['$filter', function($filter) {
+      return function(input) {
+        return "img/pokemons/" + $filter('normalize')(input) + ".jpg";
+      };
+    }]);
 
 })();
