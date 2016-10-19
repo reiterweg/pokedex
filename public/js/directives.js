5f3e969a018066570261a7134a71670065b9703d
Alejandro Diaz Carmona <alejandro.diaz@quienmepresta.com>
2016-10-19 00:16:44 -0500

    Create pokemon type directive.

--- public/js/directives.js
+++ public/js/directives.js
@@ -31,6 +31,12 @@
         templateUrl: 'partials/pokemon-evolution.html'
       };
     })
+    .directive('pokemonType', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/pokemon-type.html'
+      };
+    })
     .directive('pokemonComments', function() {
       return {
         restrict: 'E',
