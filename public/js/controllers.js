502a56682cecd939ae8b2da00c6cfb79fbee2c71
Alejandro Diaz Carmona <alejandro.diaz@quienmepresta.com>
2016-10-20 01:14:09 -0500

    Add grid and fix css.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,4 +1,4 @@
-(function() {
+(function(_) {
   angular.module('pokedex.controllers', [])
     .controller('PokedexController', ['$scope', '$routeParams', 'pokemonService', function($scope, $routeParams, pokemonService) {
       var type = $routeParams.type;
@@ -8,11 +8,19 @@
 
         pokemonService.byType(type).then(function(data) {
           $scope.pokemons = data;
+          $scope.groupped = partition(data, 4);
         });
       } else {
         pokemonService.all().then(function(data) {
           $scope.pokemons = data;
+          $scope.groupped = partition(data, 4);
         });
+      }
+
+      function partition(data, n) {
+        return _.chain(data).groupBy(function(element, index) {
+          return Math.floor(index / n);
+        }).toArray().value();
       }
     }])
     .controller('PokemonController', ['$scope', '$routeParams', 'pokemonService', function($scope, $routeParams, pokemonService) {
@@ -32,4 +40,4 @@
       };
     });
 
-})();
+})(_);
